7124df4e4db64d0bf976b29972b418cb74f6d632
vjnhat <tran.van.nhat@framgia.com>
2015-04-16 10:36:18 +0700

    finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ToyApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+  root 'microposts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,5 @@ ToyApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #root 'welcome#index'
 end
